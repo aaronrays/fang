725fb9ea23fe8d010ce0178be8090dc0445986b8
aaronrays <aaronrays780@gmail.com>
2025-05-05 21:42:42 +0530

    commit 1

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2207054.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 218769950, 218770239, 220135851, 220135858, 220137383, 220307732, 220490844, 220490846, 220636971, 220636717, 220640412, 220646411, 220646481, 220649313, 220649404, 220649912 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/218770253/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/218770254/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/218770252/1/ammo.js', 'preload' : true},
+];
